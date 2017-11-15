2a2b087f675bf41453efce2e75654d931f40e71e
Jeremy <jeraguilon@gmail.com>
2017-11-15 11:17:49 -0700

    p4

---
+++ project4/Testing.py
@@ -0,0 +1,22 @@
+from NeuralNetUtil import buildExamplesFromCarData,buildExamplesFromPenData
+from NeuralNet import buildNeuralNet
+import cPickle 
+from math import pow, sqrt
+
+def average(argList):
+    return sum(argList)/float(len(argList))
+
+def stDeviation(argList):
+    mean = average(argList)
+    diffSq = [pow((val-mean),2) for val in argList]
+    return sqrt(sum(diffSq)/len(argList))
+
+penData = buildExamplesFromPenData() 
+def testPenData(hiddenLayers = [24]):
+    return buildNeuralNet(penData,maxItr = 200, hiddenLayerList =  hiddenLayers)
+
+carData = buildExamplesFromCarData()
+def testCarData(hiddenLayers = [16]):
+    return buildNeuralNet(carData,maxItr = 200,hiddenLayerList =  hiddenLayers)
+
+testCarData()
